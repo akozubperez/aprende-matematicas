df8f963c7b348582397849843d38ca723698c35a
akozubperez <alejandra.kozubperez@gmail.com>
2019-12-09 19:05:32 +0100

    Añadida pregunta y verificación de resultado

--- src/main/resources/static/app/components/bizcocho/bizcocho.component.js
+++ src/main/resources/static/app/components/bizcocho/bizcocho.component.js
@@ -11,16 +11,37 @@
             ctrl.state = 'waiting';
             ctrl.step = 1;
             ctrl.numStep = 10;
-            var ingredientes = ['Huevos', 'Mantequilla', 'Azúcar', 'Yogur', 'Harina', 'Limón', 'Levadura'];
+            ctrl.stepEnabled = [];
+            var ingredientes = ['Huevos', 'Harina', 'Yogur', 'Azúcar', 'Levadura', 'Mantequilla', 'Limón'];
             ctrl.precios = {};
- 
+            ctrl.responseError = {};
+
             ctrl.$onInit = function () {
-                ingredientes.forEach(function (i){
-                    ctrl.precios[i] = Math.round(Math.random()*10+4);
+                var total = 0;
+                ctrl.responses = [];
+                ingredientes.filter(function (e) {
+                    return e !== 'Limón';
+                }).forEach(function (e, i) {
+                    ctrl.precios[e] = Math.round(Math.random() * 12 + 4);
+                    total += ctrl.precios[e];
+                    ctrl.responses[i + 2] = total;
                 });
+                ctrl.responses[2] = undefined;
+                for (var i = 1; i <= ctrl.numStep; i++) {
+                    ctrl.stepEnabled[i] = ctrl.responses[i] === undefined;
+                }
                 ctrl.state = 'ready';
             };
 
+            ctrl.check = function (response) {
+                if (ctrl.responses[ctrl.step] === response) {
+                    ctrl.stepEnabled[ctrl.step] = true;
+                    ctrl.responseError[ctrl.step] = false;
+                } else {
+                    ctrl.responseError[ctrl.step] = true;
+                }
+            };
+            
             ctrl.next = function () {
                 ctrl.step++;
             };
